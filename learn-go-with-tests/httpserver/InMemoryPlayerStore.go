94e479a8990fc4f8048411a2a1e2b6f3b40dfa51
Guozheng Ge <guozheng.ge@gmail.com>
2019-12-25 00:20:12 -0800

    update http server

--- learn-go-with-tests/httpserver/InMemoryPlayerStore.go
+++ learn-go-with-tests/httpserver/InMemoryPlayerStore.go
@@ -8,6 +8,14 @@ type InMemoryPlayerStore struct {
 	store map[string]int
 }
 
+func (i *InMemoryPlayerStore) GetLeague() []Player {
+	var league []Player
+	for name, wins := range i.store {
+		league = append(league, Player{name, wins})
+	}
+	return league
+}
+
 func (i *InMemoryPlayerStore) RecordWin(name string) {
 	i.store[name]++
 }
--- learn-go-with-tests/httpserver/server.go
+++ learn-go-with-tests/httpserver/server.go
@@ -1,20 +1,53 @@
 package httpserver
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+// PlayerStore stores score information about players
 type PlayerStore interface {
 	GetPlayerScore(name string) int
 	RecordWin(name string)
+	GetLeague() []Player
 }
 
+// Player stores a name with a number of wins
+type Player struct {
+	Name string
+	Wins int
+}
+
+// PlayerServer is a HTTP interface for player information
 type PlayerServer struct {
 	store PlayerStore
+	http.Handler
 }
 
-func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+const jsonContentType = "application/json"
+
+// NewPlayerServer creates a PlayerServer with routing configured
+func NewPlayerServer(store PlayerStore) *PlayerServer {
+	p := new(PlayerServer)
+
+	p.store = store
+
+	router := http.NewServeMux()
+	router.Handle("/league", http.HandlerFunc(p.leagueHandler))
+	router.Handle("/players/", http.HandlerFunc(p.playersHandler))
+
+	p.Handler = router
+
+	return p
+}
+
+func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", jsonContentType)
+	json.NewEncoder(w).Encode(p.store.GetLeague())
+}
+
+func (p *PlayerServer) playersHandler(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
 
 	switch r.Method {
